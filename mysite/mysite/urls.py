39e62e88de24867ce7c118dcf6e4a8cc4bd82d88
irgasite <25tens@naver.com>
2018-09-06 07:08:45 +0900

    modified templates css file

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('post.urls')),
